5b2ba1988ce89fd50a974b811ac0252a29f094de
maryamabdelal <82163551+maryamabdelal@users.noreply.github.com>
2026-04-22 12:12:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,20 @@ import streamlit as st
 
 home_page = st.Page(
     page="pages/home.py",
-    icon='🏡' ,
     title='Home page' ,
-  
     default=True
 )
 signin_page = st.Page( 
     page="pages/signin.py", 
     title='signin page' , 
-    icon='👥' 
 )
 signup_page = st.Page( 
     page="pages/signup.py", 
     title='signup page' , 
-    icon='🔐' 
 )
 contact_us = st.Page( 
     page="pages/contact_us.py", 
     title='Contact Us' , 
-    icon='☎️' 
 )
 
 all_pages =st.navigation(
